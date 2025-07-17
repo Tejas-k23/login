47b5fba4f8c0cd90d688126f8a8166d97834c49f
Tejas-k23 <tejaskumbharkar6398@gmail.com>
2025-07-17 14:50:57 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,18 +1,8 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
-}, {
-  timestamps: true // adds createdAt and updatedAt fields
+  username: String,
+  password: String
 });
 
-module.exports = mongoose.model("User", userSchema);
-
+module.exports = mongoose.model("User", userSchema); // collection name will be 'users'
